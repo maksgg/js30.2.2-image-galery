33eda8a619037fdaa9b614a5c592687fb16aaca0
maksgg <maks.zhovmir.97@gmail.com>
2023-10-12 13:42:34 +0300

    feat: new style and variables

---
+++ js30-2.2-image-galery/script/script.js
@@ -0,0 +1,7 @@
+const accessKey = 'zcEqAVSkkmgtfthOh8ZogG1DxWPCeozNCVNRwKbfrYg';
+
+const form = document.querySelector('form');
+const inputEl = document.getElementById('search-input');
+const magnifyingGlass = document.querySelector('.magnifying-glass');
+const searchResults = document.querySelector('.search-results');
+const showMore = document.getElementById('show-more-botton')
